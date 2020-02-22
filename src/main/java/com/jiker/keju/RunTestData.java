0c84c83797f308969ac5067869632741d0f6efe9
stonelv <zhe.lv.82@gmail.com>
2020-02-22 11:57:20 +0800

    fix the \n problem

--- src/main/java/com/jiker/keju/RunTestData.java
+++ src/main/java/com/jiker/keju/RunTestData.java
@@ -16,7 +16,7 @@ public class RunTestData {
         String result = "";
         for (String data : testDataList) {
             String[] dataArray = data.split(",");
-            result += String.format("收费%d元\\n",
+            result += String.format("收费%d元\n",
                     new TaxiCalculater().showPrice(Integer.parseInt(dataArray[0].replaceAll("\\D", "")),
                             Integer.parseInt(dataArray[1].replaceAll("\\D", ""))));
         }
--- src/test/java/com/jiker/keju/RunTestDataTest.java
+++ src/test/java/com/jiker/keju/RunTestDataTest.java
@@ -8,6 +8,6 @@ public class RunTestDataTest {
     @Test
     public void should_return_result_when_given_test_file() throws IOException {
         RunTestData  runTestData = new RunTestData("testData.txt");
-        assert runTestData.showResult().equals("收费6元\\n收费7元\\n收费13元\\n收费7元\\n");
+        assert runTestData.showResult().equals("收费6元\n收费7元\n收费13元\n收费7元\n");
     }
 }
